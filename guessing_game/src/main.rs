57f75fa0345b4d3e51b5b2b9c74c2b129ab19fb6
LHETHA <129411803+LHETHA@users.noreply.github.com>
2025-07-28 01:07:50 +0200

    v0.3.0: Update main.rs in guessing_game to receive user input and return it to the console.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io; // import io library from the standard library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();  // String is growable, UTF-8 text
+                                           // :: means that new is an associated function of String type
+
+    io::stdin()
+        .read_line(&mut guess) // &mut --> mutable (mut) reference (&)
+        .expect("Failed to read line"); // similar to expect statement in Python
+
+    println!("You guessed: {guess}"); // {} --> placeholder containing a variable
 }
