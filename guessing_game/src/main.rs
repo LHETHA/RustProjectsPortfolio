b3b8c362f6f27f25db7008cd6ac38c428e971cc9
LHETHA <129411803+LHETHA@users.noreply.github.com>
2025-07-28 01:24:37 +0200

    v.0.3.2: Update main.rs in guessing_game to generate a random number and print it via a debug statement to the console.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,19 @@
 use std::io; // import io library from the standard library
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(0..=100);
+
+    // DEBUG
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();  // String is growable, UTF-8 text
-                                           // :: means that new is an associated function of String type
+                                            // :: means that new is an associated function of String type
 
     io::stdin()
         .read_line(&mut guess) // &mut --> mutable (mut) reference (&)
