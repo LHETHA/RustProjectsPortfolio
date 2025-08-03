946413987b5c49e7c620fd4241efcf1068f7883b
LHETHA <129411803+LHETHA@users.noreply.github.com>
2025-08-03 15:09:39 +0200

    v0.4.1: Update main.rs in temperature_converter to allow conversion from Fahrenheit to Celsius.

--- temperature_converter/temperature_converter/src/main.rs
+++ temperature_converter/temperature_converter/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    fahrenheit_to_celsius();
+}
+
+// Conversion Fahrenheit to Celsius:
+// °C = (°F - 32) * 5/9
+fn fahrenheit_to_celsius() {
+    println!("Please enter your Temperature in Fahrenheit: ");
+    let mut user_input: String = String::new();
+
+    println!("Converting!");
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Error: Failed to read line.");
+    
+    let user_input: i64 = user_input.trim().parse().expect("Incorrect value.");
+
+    let converted_temperature: i64 = (user_input - 32) * 5/9;
+
+    println!("Your converted temperature is: {converted_temperature} °C");
 }
